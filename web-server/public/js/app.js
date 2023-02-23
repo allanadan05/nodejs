bfaa7dcda540d4b5bc9af56a1e3e61f405643eec
erpwebadmin5 <aadan.global@megaworldcorp.com>
2023-02-23 11:49:45 +0800

    Browser HTTP requests with Fetch; Creating a Search Form; Wiring up the UI; Setting up Heroku account and installing Heroku CLI 20230223

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,40 @@
 console.log('Client side js file is loaded');
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     });
+// });
+
+// fetch('http://localhost:3000/weather?address=manila').then((response) => {
+//     response.json().then(( {location, forecast, error=''} = {} ) => {
+//         if(error){
+//             return console.log('Error : ' + error);
+//         }
+//         console.log('Location : ' + location);
+//         console.log('Forecast : ' + forecast);
+//     });
+// });
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value.trim();
+    messageOne.textContent = "Loading...";
+    messageTwo.textContent = '';
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then(( {location, forecast, error=''} = {} ) => {
+            if(error){
+                messageOne.textContent = error;
+                return;
+            }
+            messageOne.textContent = 'Location: ' + location;
+            messageTwo.textContent = 'Forecast: ' + forecast;
+        });
+    });
+});
